e48462e6cbe543f688b7314c4cf7b7963c545c5e
Elvin <sladkiyboyka123@gmail.com>
2019-11-21 01:26:34 +0400

    Norm

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 (function($) {
     "use strict";
       $(document).on('ready', function() {
+/*
 
-
-
+*/
         jQuery(window).on('scroll', function() {
 			if ($(this).scrollTop() > 200) {
 				$('#header .header-inner').addClass("sticky");
